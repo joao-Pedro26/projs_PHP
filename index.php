3ccb56bfd248f22355e739b3f0f05824b18db83e
ldi3 <ldi3@186-217-118-157.tvu.unesp.br118.217.186.in-addr.arpa>
2025-03-11 19:14:47 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+    $dia= 06; 
+    $cidade = "<b>Bauru</b>"; 
+    $mes = 03;
+    $ano = 2025;
+
+    echo "$cidade,$dia\\$mes\\$ano"  /*imprime tudo que tem detro das variaveis
+     na ondem pre definida pelo progarmador*/
+
+?>
